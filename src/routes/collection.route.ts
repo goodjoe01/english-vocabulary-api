35d24450ebac7d0d353a62157b037d6e5efaf419
Joseph <josephgamboasanchez@gmail.com>
2023-02-25 17:10:24 -0500

    patch endpoint

--- src/routes/collection.route.ts
+++ src/routes/collection.route.ts
@@ -9,6 +9,7 @@ collectionRouter.post('/', AuthHandler, postNewcollection)
 collectionRouter.get('/', AuthHandler, getcollections)
 collectionRouter.get('/limit/:limit', AuthHandler, getLastNCollections)
 collectionRouter.get('/:id', AuthHandler, getcollection)
+collectionRouter.patch('/:id', AuthHandler, putcollection)
 collectionRouter.put('/:id', AuthHandler, putcollection)
 collectionRouter.delete('/:id', AuthHandler, deletecollection)
 collectionRouter.all('/', (req, res, next) => {
